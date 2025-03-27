8715f446b943f0499e4a5ede0cecf2ca9d8b6296
Rich182 <zhenu1276@gmail.com>
2025-03-27 14:38:55 +0200

    category index ready

--- MyMarket1/Controllers/CategoryController.cs
+++ MyMarket1/Controllers/CategoryController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyMarket1.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db) {  _db = db; }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var categories = _db.Categories.ToList();
+            return View(categories);
         }
     }
 }
